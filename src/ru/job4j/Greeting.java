a0cb6a44ca0200e69dca36ea47ba4fa0155df936
ДЕНИС <chetchelios@mail.ru>
2020-12-16 15:38:14 +0500

    9.1. String [#426949]

---
+++ src/ru/job4j/Greeting.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+public class Greeting {
+    public  static void  main(String[] args){
+        String idea = "I like Java!";
+        System.out.println(idea);
+        String ideaA = (idea + "But I am a newbie.");
+        int year = 2020;
+        System.out.println(ideaA+year);
+
+    }
+}
