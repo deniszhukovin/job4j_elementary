c0785c2c688e4eb436cb56ee51b459f276aca0ea
ДЕНИС <chetchelios@mail.ru>
2020-12-16 13:55:55 +0500

    9. Площадь треугольника. [#426945]

---
+++ src/ru/job4j/TrgArea.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+
+public class TrgArea {
+    public static double area(double a, double b, double c ) {
+        double p = (a + b + c) / 2;
+        double rsl = p * (p - a) * (p - b) * (p - c);
+        double s = Math.sqrt(rsl);
+        return s;
+    }
+    public static void main(String[] args) {
+      double s =   TrgArea.area(2,2,2);
+      System.out.println(s);
+
+    }
+
+
+}
