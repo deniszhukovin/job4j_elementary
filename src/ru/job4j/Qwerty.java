25421156f61026255d8b5064ea04e462bd41edbf
ДЕНИС <chetchelios@mail.ru>
2020-12-21 18:27:56 +0500

    ввод 56

---
+++ src/ru/job4j/Qwerty.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class Qwerty {
+    public static void jk(int b, int c) {
+        int zx = b + c;
+        System.out.print(zx);
+    }
+
+    public static void main(String[] args) {
+        Qwerty.jk(3, 4);
+
+    }
+}
