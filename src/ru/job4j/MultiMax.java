97295ee27a20c3af7e73008aab51518d45fed6aa
ИННОКЕНТИЙ <chetchelios@mail.ru>
2020-12-29 23:59:21 +0500

    4.4. Вложенный оператор if else [#426900]

---
+++ src/ru/job4j/MultiMax.java
@@ -0,0 +1,22 @@
+package ru.job4j;
+
+public class MultiMax {
+    public static int max(int first, int second, int third) {
+        int result = first;
+        if ((second > third) && (second > first)) {
+            result = second;
+            System.out.println(result);
+        } else if (third > first) {
+            result = third;
+            System.out.println(result);
+        } else {
+            System.out.println(result);
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        MultiMax.max(755, 57, 158);
+    }
+}
+
+
