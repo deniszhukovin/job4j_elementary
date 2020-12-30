5335423c0bd80bebff3d37a17ce0c80512c6a7ed
ИННОКЕНТИЙ <chetchelios@mail.ru>
2020-12-30 21:21:26 +0500

    4.6.1. Switch [#426895]

---
+++ src/ru/job4j/SwitchWeek.java
@@ -0,0 +1,37 @@
+package ru.job4j;
+
+public class SwitchWeek {
+    public static String nameOfDay(int day) {
+        String name;
+        switch (day) {
+            case 1:
+                name = "понедельник";
+                break;
+            case 2:
+                name = "вторник";
+                break;
+            case 3:
+                name = "среда";
+                break;
+            case 4:
+                name = "четверг";
+                break;
+            case 5:
+                name = "пятница";
+                break;
+            case 6:
+                name = "суббота";
+                break;
+            case 7:
+                name = "воскресенье";
+                break;
+            default:
+                name = "ошибка";
+                System.out.println(day);
+        }
+        return name;
+    }
+    static public void main(String[] args) {
+        System.out.println(SwitchWeek.nameOfDay(5));
+    }
+}
