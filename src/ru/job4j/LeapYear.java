52b3e5567c4643dcc31d1bbff86edcc4a0ce28e4
ИННОКЕНТИЙ <chetchelios@mail.ru>
2021-01-03 12:36:17 +0500

    5.0.1. Сумма чисел. [#427001]

---
+++ src/ru/job4j/LeapYear.java
@@ -0,0 +1,21 @@
+package ru.job4j;
+
+public class LeapYear {
+    public static boolean checkYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        }
+        if (year % 100 == 0) {
+            return false;
+        }
+        if (year % 4 == 0) {
+            return true;
+        }
+
+        return false;
+    }
+    public static void main(String[] args) {
+        System.out.println(checkYear(2000));
+}
+}
+
