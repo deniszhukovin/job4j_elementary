5a51c206549042401811258f8eaceec1609bbbed
ИННОКЕНТИЙ <chetchelios@mail.ru>
2021-01-13 16:17:53 +0500

    5.6. Простые числа [#426997]

--- test/ru/job4j/PrimeNumber.java
+++ test/ru/job4j/PrimeNumber.java
@@ -2,18 +2,15 @@ package ru.job4j;
 
 public class PrimeNumber {
     public static int calc(int finish) {
-    int count = 0;
-        for (int number1 = 2; number1  <= finish; number1++ ) {
-        if (CheckPrimeNumber.check(number1)) {
-            boolean prime = true;
-            count++ ;
+        int count = 0;
+        for (int number = 2; number <= finish; number++) {
+            if (CheckPrimeNumber.check(number)) {
+                boolean prime = true;
+                count++;
+            }
         }
-    }
         return count;
+    }
 }
 
-    public static void main(String[] args) {
-        System.out.println(calc(5));
-    }
-}
 
