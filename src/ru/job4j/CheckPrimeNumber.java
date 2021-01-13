17379e7315e4dfc080f18b621ffdb2443cda46d0
ИННОКЕНТИЙ <chetchelios@mail.ru>
2021-01-13 15:14:19 +0500

    5.4. Протеиновая диета [#426996]

---
+++ src/ru/job4j/CheckPrimeNumber.java
@@ -0,0 +1,24 @@
+package ru.job4j;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean prime = true;
+        for (int ind = 2; ind < (number - 1); ind++) {
+             if (number % ind == 0) {
+                prime = false;
+                break;
+            }
+        }
+        return prime;
+    }
+}
+
+
+
+
+
+
+
+
+
+
