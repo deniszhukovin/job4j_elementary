0f4ba79dde50b7b4c0f7a95e5887438aa2d13a28
ИННОКЕНТИЙ <chetchelios@mail.ru>
2021-01-14 20:54:18 +0500

    5.7. Ипотека [#426998]

---
+++ src/ru/job4j/Mortgage.java
@@ -0,0 +1,16 @@
+package ru.job4j;
+
+public class Mortgage {
+    public static int year(double amount, int salary, double percent) { // int amount будет в методе с потерей данных//
+        int year = 0;
+        while (amount >= 0) {
+            amount = amount + amount * percent / 100 - salary; // процент нужно иначе вычислять?//
+            System.out.println(year);
+            year++;
+        }
+            return year;
+        }
+        public static void main(String[] args) { Mortgage.year(100, 70, 50); }
+
+
+}
