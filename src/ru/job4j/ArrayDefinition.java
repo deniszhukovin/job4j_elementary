db54ec3bbc9b7e91eb27ff91ae30d2d8219d04a7
ИННОКЕНТИЙ <chetchelios@mail.ru>
2021-01-20 17:24:19 +0500

    6.0.1.1. Размер массива [#426931] 6.0.1.1. Размер массива [#426931] 6.0.1.1. Размер массива [#426931]

--- src/ru/job4j/ArrayDefinition.java
+++ src/ru/job4j/ArrayDefinition.java
@@ -1,8 +1,12 @@
 package ru.job4j;
 
 public class ArrayDefinition {
-short ages[] = new short[10];
-String surnames[] = new String[100500];
-float prices[] = new float[40];
-
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+        System.out.println("размер массива равен :" + ages.length);
+        System.out.println("размер массива равен :" + surnames.length);
+        System.out.println("размер массива равен :" + prices.length);
+    }
 }
