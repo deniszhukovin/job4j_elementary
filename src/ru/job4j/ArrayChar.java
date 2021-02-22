436065cc26fcb25ee104427bc3d3b8e5f27314e6
ИННОКЕНТИЙ <chetchelios@mail.ru>
2021-02-22 21:46:45 +0500

    6.4. Слово начинается с ... [#426908]

--- src/ru/job4j/ArrayChar.java
+++ src/ru/job4j/ArrayChar.java
@@ -3,7 +3,14 @@ package ru.job4j;
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
+            for (int in = 0; in < pref.length; in++) {
+                if (pref[in] != word[in]) {
+                    result = false;
+                    break;
+                }
+            }
 
         return result;
     }
 }
+
