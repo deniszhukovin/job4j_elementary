16128250f9a6e8544090f5c5819fc00d3a080734
ИННОКЕНТИЙ <chetchelios@mail.ru>
2021-02-22 21:51:45 +0500

    6.3. Массив заполнен true или false [#426909]

--- src/ru/job4j/Check.java
+++ src/ru/job4j/Check.java
@@ -3,8 +3,8 @@ package ru.job4j;
 public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = true;
-       for (int index = 0; index < data.length; index++) {
-           if (data[0] != data[index]){
+       for (int ind = 0; ind < data.length;ind++) {
+           if (data[0] != data[ind]){
                result = false;
                break;
            }
